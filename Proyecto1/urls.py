9582c8e2763d50cdff67caa2fe77713419ba9da0
Khioya <edgar_eomc@hotmail.com>
2025-06-11 11:36:54 -0600

    Ajuste favico.ico

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', serve, {'path': 'favicon.ico', 'document_root': settings.STATICFILES_DIRS[0]}),
 ]
